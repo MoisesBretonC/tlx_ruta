627ebd3390149b1fcf030cd2d2f295ba81de8a65
DeividDDHXT <jestmon02@gmail.com>
2025-03-17 10:57:04 -0600

    CheckPoint

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,7 +3,7 @@ import axios from './axios.js';
 
 const API = 'http://localhost:4000/api'
 
-export const registerRequest = user => axios.post(`/register`, user);
+export const registerRequest = user => axios.post(`${API}/register`, user);
 
 export const loginRequest = user => axios.post(`/login`, user);
 
