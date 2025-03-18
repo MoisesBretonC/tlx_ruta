91cbd5396da03daff4fc5f0f15ed274a8a28dea1
DeividDDHXT <jestmon02@gmail.com>
2025-03-18 10:35:02 -0600

    Copia de seguridad2

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { loginRequest } from "../api/auth";
+import { registerRequest,loginRequest } from "../api/auth";
 import Cookie from "js-cookie";
 
 const AuthContext = createContext();
