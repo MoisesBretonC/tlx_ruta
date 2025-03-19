443f929c74fe325c1407c7649383ae498b56a561
Hinmorlath <josepolomanuel2@gmail.com>
2025-03-19 10:25:17 -0600

    Agregando el Inicio de la página

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,47 @@
-import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function HomePage() {
-  const navigate = useNavigate();
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold mb-6 text-black">HomePage</h1>
-      <button
-        onClick={() => navigate("/register")}
-        className="px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-600 transition"
-      >
-        Ir a Registro
-      </button>
+    <>
+    <Navbar />
+    {/* Descripción */}
+    <div style={{background: "#fff", color:"#000"}}>
+      <section className="max-w-4xl mx-auto p-6">
+        <h2 className="text-2xl font-semibold mb-4">¿Qué es TLX-Ruta?</h2>
+        <p>
+          TLX-Ruta es un sistema de gestión diseñado para concesionarios de transporte público.
+          Permite administrar corporativos, operadores, rutas, unidades, horarios y turnos de manera eficiente.
+        </p>
+      </section>
+        
+      {/* /* Características */}
+      <section className="max-w-4xl mx-auto p-6">
+        <h2 className="text-2xl font-semibold mb-4">Características principales</h2>
+        <ul className="list-disc pl-6 space-y-2">
+          <li>Gestión de personal y operadores</li>
+          <li>Administración de rutas, horarios y turnos</li>
+          <li>Creación de corporativos y registro de unidades</li>
+          <li>Asignación de operadores a rutas y unidades</li>
+        </ul>
+      </section>
+        
+      {/* Tecnologías */}
+      <section className="max-w-4xl mx-auto p-6">
+        <h2 className="text-2xl font-semibold mb-4">Tecnologías utilizadas</h2>
+        <div className="flex space-x-4"  style={{color: "#000"}}>
+          <span className="bg-blue-200 px-4 py-2 rounded-lg">React</span>
+          <span className="bg-green-200 px-4 py-2 rounded-lg">Node.js</span>
+          <span className="bg-gray-200 px-4 py-2 rounded-lg">Express.js</span>
+          <span className="bg-yellow-200 px-4 py-2 rounded-lg">MongoDB</span>
+        </div>
+      </section>
+        
+      {/* Empresa */}
+      <footer className="text-center py-6 bg-gray-800 text-white mt-6">
+        <p>Desarrollado por <strong>Sof-TI</strong></p>
+      </footer>
     </div>
+    </>
   );
 }
 
